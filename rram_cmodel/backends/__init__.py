70d7b0d4162c61a1ce41c4e5276fa6e6981d3386
Xuliang <88468868+DingdongD@users.noreply.github.com>
2026-08-11 09:39:07 +0800

    align backend public interfaces

--- rram_cmodel/backends/__init__.py
+++ rram_cmodel/backends/__init__.py
@@ -1,4 +1,13 @@
-from .destiny import DestinyParameterAdapter
-from .ramulator2 import Ramulator2Adapter
+from .destiny import DestinyManifestBuilder, DestinyParameterAdapter
+from .ramulator2 import Ramulator2Adapter, Ramulator2Run
+from .ramulator21 import Ramulator21HBM3Adapter, Ramulator21HBM3Config, Ramulator21Run
 
-__all__ = ["DestinyParameterAdapter", "Ramulator2Adapter"]
+__all__ = [
+    "DestinyManifestBuilder",
+    "DestinyParameterAdapter",
+    "Ramulator2Adapter",
+    "Ramulator2Run",
+    "Ramulator21HBM3Adapter",
+    "Ramulator21HBM3Config",
+    "Ramulator21Run",
+]
